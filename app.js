56c050233d418d513080d6283fd87947f4f33ee6
Jwg3full <jonathapneus@gmail.com>
2025-11-21 17:01:45 -0300

    projeto iniciado

---
+++ app.js
@@ -0,0 +1,14 @@
+let listaGenerica = ["js","c","c++","kotlin","py;"];
+listaGenerica.push("java", "Ruby","GoLang" );
+console.log(listaGenerica);
+
+let listNome = ["joao", "rafa", "anita"];
+
+console.log(listNome[1]);
+
+
+
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
